2f0e33796b456478ebef7ddeb071a196b60aab5f
CousiGoico <javier.cousino.goicolea@hotmail.com>
2024-02-13 07:01:56 +0100

    Change

--- Form1.cs
+++ Form1.cs
@@ -19,32 +19,4 @@ public partial class Form1 : Form
         this.Controls.Add(CreateGroupBox(this.Width - 300, this.Height, 300, 0));
     }
 
-    private GroupBox CreateGroupBox(int width, int height, int left, int top){
-        var container = new GroupBox();
-        container.Width = width;
-        container.Height = height;
-        container.Top = top;
-        container.Left = left;
-        return container;
-    }
-
-    private MenuStrip CreateMenuStrip(){
-        var mainMenu = new MenuStrip();
-        mainMenu.Items.Add("Archivo");
-        return mainMenu;
-    }
-
-    private ToolStrip CreateToolbar(){
-        var toolbar = new ToolStrip();
-        ToolStripButton button = new ToolStripButton();
-        button.Text = "Cerrar";
-        //button.Click += new EventHandler();
-        toolbar.Items.Add(button);
-        return toolbar;
-    }
-
-    private StatusStrip CreateStatusStrip(){
-        return new StatusStrip();
-    }
-    
 }
