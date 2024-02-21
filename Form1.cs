293a3f177affc67e82f00c58a674a47a5068c63e
CousiGoico <javier.cousino.goicolea@hotmail.com>
2024-02-21 19:03:17 +0100

    GitIgnore

--- Form1.cs
+++ Form1.cs
@@ -11,12 +11,12 @@ public partial class Form1 : Form
         // textbox.Top = 0;
         // textbox.Left = 0;
         // textbox.Width = 200;
-        this.Controls.Add(CreateToolbar());
-        this.Controls.Add(CreateMenuStrip());
+        this.Controls.Add(Designer.CreateToolbar());
+        this.Controls.Add(Designer.CreateMenuStrip());
         
-        this.Controls.Add(CreateStatusStrip());
-        this.Controls.Add(CreateGroupBox(300, this.Height, 0, 0));
-        this.Controls.Add(CreateGroupBox(this.Width - 300, this.Height, 300, 0));
+        this.Controls.Add(Designer.CreateStatusStrip());
+        this.Controls.Add(Designer.CreateGroupBox(300, this.Height, 0, 0));
+        this.Controls.Add(Designer.CreateGroupBox(this.Width - 300, this.Height, 300, 0));
     }
 
 }
